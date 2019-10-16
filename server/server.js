07a075f5efcb156e7f5598f7b782494893ffc850
YuanZheng Hu <bhu078@uottawa.ca>
2019-10-16 03:37:27 -0400

    dis and connect event listener

--- server/server.js
+++ server/server.js
@@ -1,12 +1,32 @@
 const path = require('path')
+const http = require('http')
 
+const socketIO = require('socket.io')
 const express = require('express')
 const publicPath = path.join(__dirname, '/../public')
 const port = process.env.PORT || 3000 
 var app = express()
 
+var server = http.createServer(app)
+var io = socketIO(server)
+
+
 app.use(express.static(publicPath))
 
-app.listen(port,()=>{
+
+io.on('connection',(socket)=>{
+    console.log("A new user just connected")
+
+
+    socket.on('disconnect',(socket)=>{
+        console.log("User was disconnected")
+    })
+
+})
+
+
+
+
+server.listen(port,()=>{
     console.log(`Server is up on port ${port}`)
 })
