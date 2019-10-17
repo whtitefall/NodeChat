76962641ad513b629f99f8d307ccbdd6a884594d
YuanZheng Hu <bhu078@uottawa.ca>
2019-10-16 20:59:49 -0400

    boardcasting event

--- server/server.js
+++ server/server.js
@@ -18,9 +18,43 @@ io.on('connection',(socket)=>{
     console.log("A new user just connected")
 
 
+        socket.on('createMessage',(message)=>{
+        console.log('Create message',message)
+        io.emit('newMessage',{
+            from: message.from,
+            text: message.text, 
+            createdAt: new Date().getTime()
+        } )
+        })
+        
+        socket.emit('newMessage',{
+            from: "admin",
+            text: "Welcome to the chat app", 
+            createdAt: new Date().getTime()
+        })
+
+        socket.broadcast.emit('newMessage',{
+            from : "Admin", 
+            text: "new user joined",
+            createdAt: new Date().getTime()
+        })
+
+        // socket.broadcast.emit('newMessage',{
+        //         from : message.from,
+        //         text: message.text,
+        //         createdAt: new Date().getTime()
+        //     })
+   
+
+
+
     socket.on('disconnect',(socket)=>{
         console.log("User was disconnected")
     })
+
+
+
+
 
 })
 
